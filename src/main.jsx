51cc2cd9d5a9080961deef502df246c7c7a6e0d9
beatrizdaddea <beatrizchith@gmail.com>
2025-10-06 21:52:20 -0300

    feat: implement main app logic

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+
+import HomePage from './Pages/Homepage'
+import CheckoutPage from './Pages/Checkout'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/checkout",
+    element: <CheckoutPage />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
